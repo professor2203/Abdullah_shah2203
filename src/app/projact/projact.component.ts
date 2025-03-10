7bba5f73cf7179876716690ef23ff162ea0205bb
Nazia <1455490aptechiic@gmail.com>
2025-03-10 16:29:14 +0500

    create education page

--- src/app/projact/projact.component.ts
+++ src/app/projact/projact.component.ts
@@ -7,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrl: './projact.component.css'
 })
 export class ProjactComponent {
-
+  projects = [
+    { name: 'Capture', image: 'Capture.JPG', link: 'https://code-dazzle.vercel.app/' },
+    { name: 'Code Dazel', image: 'code dazel.JPG', link: 'https://level5-rho.vercel.app/' },
+    { name: 'Mafic', image: 'mafic.JPG', link: 'https://mefic-tau.vercel.app/' },
+    { name: 'Travel CMS', image: 'travel.JPG', link: 'https://travel-cms-blue.vercel.app/' }
+  ];
 }
